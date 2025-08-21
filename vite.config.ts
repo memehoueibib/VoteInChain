e524a04cf8acd37f20dcaafdc1d662a74df6c06b
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2025-08-22 01:16:15 +0200

    Fix : Railway conflict with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
